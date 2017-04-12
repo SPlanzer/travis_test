0cf5149c685fee45be5a8d5879384171caa856d7
Splanzer <splanzer@linz.govt.nz>
2017-04-12 12:35:42 +1200

    test

--- ldsplugin/QgisLdsPlugin_dialog.py
+++ ldsplugin/QgisLdsPlugin_dialog.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- QgisLdsPlugin
+ QgisLdsPluginDialog
                                  A QGIS plugin
  Import LDS OGC Datasets into QGIS
                              -------------------
         begin                : 2017-04-07
+        git sha              : $Format:%H$
         copyright            : (C) 2017 by Land Information New Zealand
         email                : splanzer@linz.govt.nz
-        git sha              : $Format:%H$
  ***************************************************************************/
 
 /***************************************************************************
@@ -19,17 +19,23 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
 """
 
+import os
 
-# noinspection PyPep8Naming
-def classFactory(iface):  # pylint: disable=invalid-name
-    """Load QgisLdsPlugin class from file QgisLdsPlugin.
+from PyQt4 import QtGui, uic
 
-    :param iface: A QGIS interface instance.
-    :type iface: QgsInterface
-    """
-    #
-    from .QgisLdsPlugin import QgisLdsPlugin
-    return QgisLdsPlugin(iface)
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'QgisLdsPlugin_dialog_base.ui'))
+
+
+class QgisLdsPluginDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(QgisLdsPluginDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
